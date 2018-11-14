b8726cf5272a028d6d2e9a4738c6cb5eb8bc4363
Student <example@example.com>
2018-11-14 13:38:26 -0800

    generate a number line and pass test. Build test method for function number 2

---
+++ Ping-Pong.Tests/ModelTests/Ping-Pong.Tests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using NumberRange;
+
+namespace NumberRange.Tests
+{
+  [TestClass]
+  public class NumberTest
+  {
+    [TestMethod]
+    public void Numbers_NumberLineGenerates_True()
+    {
+      PingPong testPingPong = new PingPong();
+      List<int> expectedResultList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+      var actualResult =  testPingPong.NumberRange(2);
+      CollectionAssert.AreEqual(expectedResultList, actualResult);
+    }
+
+    [TestMethod]
+    public void Numbers_NumberDivisibleBy3_True()
+    {
+      PingPong testPingPong = new PingPong();
+      List<int> expectedResultList = new List<int> { 1, 2};
+      var actualResult =  testPingPong.NumberRange(3);
+      CollectionAssert.AreEqual(expectedResultList, actualResult);
+
+    }
+  }
+}
