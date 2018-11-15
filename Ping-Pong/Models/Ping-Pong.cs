3a80d577b7969891d83bcf0afa824b1fa157bc22
Student <example@example.com>
2018-11-14 16:55:01 -0800

    fix test methods and compilation issues

--- Ping-Pong/Models/Ping-Pong.cs
+++ Ping-Pong/Models/Ping-Pong.cs
@@ -1,12 +1,35 @@
 using System;
+using System.Collections.Generic;
 
-namespace NumberRange
+namespace NumberList
 {
   public class PingPong
   {
-    // public bool Numbers(int number)
-    // {
-    //
-    // }
+    public List<string> NumberRange(int number)
+    {
+      List<string> testList = new List<string> {};
+      for (int i = 1; i <= number; i++)
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        string step3 = i.ToString("PiNg-PoNg");
+        testList.Add(step3);
+      }
+      else if (i % 5 == 0)
+      {
+        string step2 = i.ToString("pong");
+        testList.Add(step2);
+      }
+      else if(i % 3 == 0)
+      {
+        string step1 = i.ToString("ping");
+        testList.Add(step1);
+      }
+      else
+      {
+        testList.Add(i.ToString());
+      }
+
+      return testList;
+    }
   }
 }
--- Ping-Pong/Program.cs
+++ Ping-Pong/Program.cs
@@ -2,26 +2,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace NumberRange
+namespace NumberList
 {
   public class Program
   {
     public static void Main()
     {
 
-      //This is where the number list should be. It should use the Enumerable.Range method to generate a range. Enumerable.Range(takes the value of the first integer which will be hard coded to 1, takes the number of sequential integers to generate which will be hard coded to a user input)
       Console.WriteLine("Enter a number to create a range");
       string userInputString = Console.ReadLine();
       int userInputInt = int.Parse(userInputString);
       Console.WriteLine(" ");
+      PingPong myPingPong = new PingPong();
+      List<string> pingpong = myPingPong.NumberRange(userInputInt);
+      Console.WriteLine(pingpong);
 
-      IEnumerable<int> numberList = Enumerable.Range(1, userInputInt);
 
-      foreach (int num in numberList)
-      {
-    
-        Console.WriteLine(num);
+
       }
     }
   }
-}
